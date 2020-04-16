a1f21129d45a809a68dedc3b5017f71492559513
Jiawei Mao <maojiawei97@hotmail.com>
2020-04-17 00:57:13 +0800

    TD5

--- src/main/groovy/gl51/td3/service/IpConverterService.groovy
+++ src/main/groovy/gl51/td3/service/IpConverterService.groovy
@@ -1,5 +1,4 @@
 package gl51.td3.service
-import gl51.td3.data.Ip
 
 interface IpConverterService {
 
--- src/test/groovy/gl51/td3/MovieControllerSpec.groovy
+++ src/test/groovy/gl51/td3/MovieControllerSpec.groovy
@@ -3,7 +3,7 @@ import gl51.td3.data.MovieRequest
 import gl51.td3.movie.data.Movie
 import gl51.td3.movie.service.impl.MovieClientImpl
 import gl51.td3.movie.service.impl.MovieRegistryImpl
-import gl51.td3.movie.service.MovieRegistry
+
 import gl51.td3.movie.service.MovieClient
 import io.micronaut.core.type.Argument
 import io.micronaut.http.HttpRequest
@@ -15,7 +15,7 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.test.annotation.MockBean
 import io.reactivex.Flowable
-import io.reactivex.Maybe
+
 import spock.lang.AutoCleanup
 import spock.lang.Specification
 import spock.lang.Shared
@@ -33,7 +33,7 @@ class MovieControllerSpec extends Specification {
     @Inject
     MovieRegistryImpl registry
 
-    void "test index"() {
+    void "testIndex"() {
         given:
         Flowable flowable = client.retrieve(HttpRequest.GET("/movie"), Argument.listOf(Movie))
         def content = flowable.firstElement()
@@ -43,7 +43,7 @@ class MovieControllerSpec extends Specification {
 
 
 
-    void "test film creation"() {
+    void "testFilmCreation"() {
         given:
         HttpResponse response = client.toBlocking().exchange(
                 HttpRequest.POST("/movie", new MovieRequest(imdbID: "zhk"))
